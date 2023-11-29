e3404574b44784d3edf53b01ae082cb2ae575c85
MokhatlaM <119861055+MokhatlaM@users.noreply.github.com>
2023-11-29 10:02:40 +0200

    MongoDB connection

--- API/server/index.js
+++ API/server/index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
+const connect = require("./config/db");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -10,4 +12,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
-app.lest
+connect();
+
+mongoose.connection.once("open", () => {
+  console.log("connected to String Smith Database");
+  app.listen(PORT, () => console.log(`server is running on ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+});
